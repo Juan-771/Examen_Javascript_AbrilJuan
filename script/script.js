cf0956f582dc15ececbb9836254a8211124df8fa
Juan-771 <abrilromanj@gmail.com>
2025-05-26 09:06:30 -0500

    feat: :sparkles: Search Comida

--- script/script.js
+++ script/script.js
@@ -1,19 +1,15 @@
 
 
-document.getElementById("datica").addEventListener("submit", function (a) {
-    a.preventDefault();
-
+document.getElementById("datica").addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    console.log("el evento si a sucedido")
     const comidita = document.getElementById("busqueda").value
 
     localStorage.setItem("comida", comidita);
     console.log(comidita)
-    if (comidita == "a"){
-        async function llamadaAPI(){
-            let response = await fetch("www.themealdb.com/api/json/v1/1/search.php?f=a")
-            let data = await response.json()
-            console.log(data)
-            mostrarComida(data)
-        
+    if (comidita === "a"){
+
         }
     }
- } )
+ )
+ 
